aec4299cd8a1d2d437ee532a06fa0e7c66d27d9a
Harrison Bostock <harrison@bostockgroup.local@code-server.bostockgroup.local>
2023-03-30 02:07:04 +0000

    Updating Main

---
+++ main.py
@@ -0,0 +1,18 @@
+import imp
+from pydoc import doc
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import QCoreApplication
+from PyQt6.QtGui import QPixmap
+from Ui_hangman import Ui_MainWindow
+from datastore import Datastore
+
+class MainWindow
+    def __init__(self):
+        # Init UI elements
+        self.main_win = QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.main_win)
+        # Init App vars
+        Self.db = Datastore()
+
