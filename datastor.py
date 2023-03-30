c8019717d5f79b433731d84874b7900123108343
Harrison Bostock <harrison@bostockgroup.com>
2023-03-30 02:18:31 +0000

    added db fundementals

--- datastor.py
+++ datastor.py
@@ -10,8 +10,16 @@ class Datastore():
         db_file = "RTR_db.db"
         self.conn = sqlite3.connect(db_file)
         self.cur = self.conn.cursor()
+    def __del__(self):
+        self.connection.close()
+    
+    def build_db(self):
 
-
+        self.cursor.execute(
+                """
+                CREATE TABLE patient(
+                    name TEXT NOT NULL)
+                """
 # Get methods
 
 
