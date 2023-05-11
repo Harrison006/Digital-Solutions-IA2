0a480bbb2e962a3a93a83ceb3830c60b3b52175f
Harrison Bostock <87344112+Harrison006@users.noreply.github.com>
2023-05-11 19:43:20 +1000

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -4,13 +4,11 @@ from PyQt6.QtCore import QDateTime, QDate, QTime, Qt
 
 
 class Datastore:
-    def __init__(self, db_file_name: str):
+    def __init__(self):
+        db_file = "road_to_recovery_last.db"
+        self.connection = sqlite3.connect(db_file)
 
-        self.connection = sqlite3.connect(db_file_name)
         self.cursor = self.connection.cursor()
-
-        self.build_db()
-        self.populate_db()
 
     def __del__(self):
         self.connection.close()
@@ -59,7 +57,7 @@ class Datastore:
         self.cursor.execute(
             """
             CREATE TABLE exercises_tb(
-            exercies_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            exercises_id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL, 
             target TEXT NOT NULL, 
             equipment TEXT NOT NULL, 
@@ -85,7 +83,7 @@ class Datastore:
             exercise_id TEXT NOT NULL,
             reps TEXT NOT NULL,
             PRIMARY KEY (exercise_regime_id,exercise_id),
-            FOREIGN KEY (exercise_regime_id) REFERENCES exercise_regime_tb(exercise_regime_id),
+           FOREIGN KEY (exercise_regime_id) REFERENCES exercise_regime_tb(exercise_regime_id),
             FOREIGN KEY (exercise_id) REFERENCES exercises_tb(exercise_id)
             )
             """
@@ -96,7 +94,7 @@ class Datastore:
             CREATE TABLE speech_assessments_score_tb(
             speech_assessments_score INTEGER PRIMARY KEY AUTOINCREMENT,
             aphasia_score INTERGER NOT NULL,
-            aprazia_score INTERGER NOT NULL,
+            apraxia_score INTERGER NOT NULL,
             dysathria_score INTERGER NOT NULL,
             dysphoria_score INTERGER NOT NULL,
             memory_score INTERGER NOT NULL,
@@ -156,7 +154,7 @@ class Datastore:
                         )
 
                 self.connection.commit()
-        with open("patient_data_2023.csv", encoding="utf-8") as patient_data:
+        with open("patient_data.csv", encoding="utf-8") as patient_data:
             csv_reader = csv.DictReader(patient_data, delimiter=",")
 
             for record in csv_reader:
@@ -185,7 +183,7 @@ class Datastore:
                     patient_id = self.get_patient_id(first_name, last_name)
 
                     time = 0
-                    clinician_id = 0
+                    clinician_id = 1
                     date = record["appointment_date"]
                     try:
                         self.add_appointment(date, time, clinician_id, patient_id)
@@ -193,7 +191,6 @@ class Datastore:
                         with open("error_log.txt", "a", encoding="utf-8") as log:
                             log.write(f"idek\n")
                     appointment_id = self.get_appointment_id(date, patient_id)
-                    print(appointment_id)
 
                 if record["id"]:
                     self.add_appointment_physio_data(
@@ -418,7 +415,7 @@ class Datastore:
                                         elbow_flexors_score,
                                         elbow_extensors_score,
                                         wrist_extensors_score,
-                                        finger_flexors_score,
+                                       finger_flexors_score,
                                         hand_intrinsics_score,
                                         appointment_id)
             VALUES(:shoulder_abductors_score,
@@ -510,6 +507,140 @@ class Datastore:
         )
         self.connection.commit()
 
+    def add_physio_results(
+        self,
+        patient_id,
+        appointment_date,
+        hip_flexor_score,
+        knee_flexor_score,
+        dorsiflexor_score,
+        plantar_flexor_score,
+    ):
+        appointment_id = self.get_appointment_id(appointment_date, patient_id)
+        self.cursor.execute(
+            """
+               INSERT INTO physio_assessments_score_tb(hip_flexor_score,knee_flexor_score,dorsiflexor_score,plantar_flexor_score,appointment_id) 
+               VALUES(:hip_flexor_score,:knee_flexor_score,:dorsiflexor_score,:plantar_flexor_score,:appointment_id)
+            """,
+            {
+                "hip_flexor_score": hip_flexor_score,
+                "knee_flexor_score": knee_flexor_score,
+                "dorsiflexor_score": dorsiflexor_score,
+                "plantar_flexor_score": plantar_flexor_score,
+                "appointment_id": appointment_id,
+            },
+        )
+        self.connection.commit()
+
+    def get_physio_results(self, appointment_id):
+        self.cursor.execute(
+            """
+            SELECT hip_flexor_score,knee_flexor_score,dorsiflexor_score,plantar_flexor_score
+            FROM physio_assessments_score_tb
+            WHERE appointment_id = :appointment_id
+            """,
+            {"appointment_id": appointment_id},
+        )
+        results = self.cursor.fetchall()
+        return results
+
+    def get_ot_results(self, appointment_id):
+        self.cursor.execute(
+            """
+            SELECT shoulder_abductors_score,elbow_flexors_score,elbow_extensors_score,wrist_extensors_score,finger_flexors_score,hand_intrinsics_score
+            FROM ot_assessments_score_tb
+            WHERE appointment_id = :appointment_id
+            """,
+            {"appointment_id": appointment_id},
+        )
+        results = self.cursor.fetchall()
+        return results
+
+    def get_speech_results(self, appointment_id):
+        self.cursor.execute(
+            """
+            SELECT aphasia_score,apraxia_score,dysathria_score,dysphoria_score,memory_score,attention_score,judgment_score,neglect_score
+            FROM speech_assessments_score_tb
+            WHERE appointment_id = :appointment_id
+            """,
+            {"appointment_id": appointment_id},
+        )
+        results = self.cursor.fetchall()
+        return results
+
+    def add_speech_results(
+        self,
+        patient_id,
+        appointment_date,
+        aphasia_score,
+        apraxia_score,
+        dysathria_score,
+        dysphoria_score,
+        memory_score,
+        attention_score,
+        judgment_score,
+        neglect_score,
+    ):
+        appointment_id = self.get_appointment_id(appointment_date, patient_id)
+        self.cursor.execute(
+            """
+            INSERT INTO speech_assessments_score_tb(aphasia_score,apraxia_score,dysathria_score,dysphoria_score,memory_score,attention_score,judgment_score,neglect_score,appointment_id)
+            VALUES(:aphasia_score,:apraxia_score,:dysathria_score,:dysphoria_score,:memory_score,:attention_score,:judgment_score,:neglect_score,:appointment_id)
+            """,
+            {
+                "aphasia_score": aphasia_score,
+                "apraxia_score": apraxia_score,
+                "dysathria_score": dysathria_score,
+                "dysphoria_score": dysphoria_score,
+                "memory_score": memory_score,
+                "attention_score": attention_score,
+                "judgment_score": judgment_score,
+                "neglect_score": neglect_score,
+                "appointment_id": appointment_id,
+            },
+        )
+        self.connection.commit()
+
+    def make_regime(self, patient_id, date):
+        self.cursor.execute(
+            """
+            INSERT INTO exercise_regime_tb(patient_id,date)
+            VALUES(:patient_id,:date)
+            """,
+            {"patient_id": patient_id, "date": date},
+        )
+        self.connection.commit()
+
+    def get_regime_id(self, patient_id, date):
+        self.cursor.execute(
+            """
+            SELECT exercise_regime_id
+            FROM exercise_regime_tb
+            WHERE patient_id = :patient_id AND date = :date
+            """,
+            {"patient_id": patient_id, "date": date},
+        )
+        results = self.cursor.fetchone()
+
+        if results is None:
+            return None
+        else:
+            return results[0]
+
+    def insert_excerise_regime(self, exercise_regime_id, exercise_id, reps):
+        self.cursor.execute(
+            """
+            INSERT INTO exercise_regime_exercises_tb(exercise_regime_id,exercise_id,reps)
+            VALUES(:exercise_regime_id,:exercise_id,:reps)
+            """,
+            {
+                "exercise_regime_id": exercise_regime_id,
+                "exercise_id": exercise_id,
+                "reps": reps,
+            },
+        )
+        self.connection.commit()
+
     def get_password(self, username):
         self.cursor.execute(
             """
@@ -541,3 +672,201 @@ class Datastore:
             return None
         else:
             return results[0]
+
+    def get_amount_of_appointments(self, patient_id):
+        self.cursor.execute(
+            """
+            SELECT COUNT(appointment_id)
+            FROM appointment_tb
+            WHERE patient_id = :patient_id
+            """,
+            {"patient_id": patient_id},
+        )
+        results = self.cursor.fetchone()
+
+        if results is None:
+            return None
+        else:
+            return results[0]
+
+    def get_appointments(self, patient_id):
+        self.cursor.execute(
+            """
+            SELECT date
+            from appointment_tb
+            WHERE patient_id = :patient_id
+            """,
+            {"patient_id": patient_id},
+        )
+        results = self.cursor.fetchall()
+        processed = []
+        for results in results:
+            processed.append(results[0])
+        return processed
+
+    def get_appointments_time(self, patient_id):
+        self.cursor.execute(
+            """
+            SELECT time
+            from appointment_tb
+            WHERE patient_id = :patient_id
+            """,
+            {"patient_id": patient_id},
+        )
+        results = self.cursor.fetchall()
+        processed = []
+        for results in results:
+            processed.append(results[0])
+        return processed
+
+    def get_amount_of_previous_regimes(self, patient_id):
+        self.cursor.execute(
+            """
+            SELECT COUNT(date)
+            FROM exercise_regime_tb
+            WHERE patient_id = :patient_id
+            """,
+            {"patient_id": patient_id},
+        )
+        results = self.cursor.fetchone()
+
+        if results is None:
+            return None
+        else:
+            return results[0]
+
+    def get_previous_regimes(self, patient_id):
+        self.cursor.execute(
+            """
+            SELECT date
+            FROM exercise_regime_tb
+            WHERE patient_id = :patient_id
+            """,
+            {"patient_id": patient_id},
+        )
+        results = self.cursor.fetchall()
+        processed = []
+        for results in results:
+            processed.append(results[0])
+        return processed
+
+    def get_admin_staff(self, username, password):
+        self.cursor.execute(
+            """
+            SELECT practice_administrator
+            FROM clinician_tb
+            WHERE username = :username AND password = :password
+            """,
+            {"username": username, "password": password},
+        )
+        results = self.cursor.fetchone()
+        return results[0]
+
+    def add_clinician(self, name, username, password, clinician_type, admin):
+        self.cursor.execute(
+            """
+            INSERT INTO clinician_tb(name,username,password,clinician,practice_administrator)
+            VALUES(:name,:username,:password,:clinician,:practice_administrator)
+
+            """,
+            {
+                "name": name,
+                "username": username,
+                "password": password,
+                "clinician": clinician_type,
+                "practice_administrator": admin,
+            },
+        )
+        self.connection.commit()
+
+    def get_appointments_clinician(self, clinician_id):
+        self.cursor.execute(
+            """
+            SELECT date
+            FROM appointment_tb
+            WHERE clinician_id = :clinician_id
+            """,
+            {"clinician_id": clinician_id},
+        )
+        results = self.cursor.fetchall()
+        processed = []
+        for results in results:
+            processed.append(results[0])
+        return processed
+
+    def get_appoint_time(self, clinician_id, appointments):
+        self.cursor.execute(
+            """
+            SELECT time 
+            FROM appointment_tb
+            WHERE clinician_id = :clinician_id AND date = :date
+            """,
+            {"clinician_id": clinician_id, "date": appointments},
+        )
+        results = self.cursor.fetchone()
+        return results[0]
+
+    def get_exercises(self, regime_id):
+        self.cursor.execute(
+            """
+            SELECT t2.name, t1.reps
+            FROM exercise_regime_exercises_tb as t1
+            INNER JOIN exercises_tb as t2
+            ON t2.exercises_id = t1.exercise_id
+            WHERE t1.exercise_regime_id = :regime_id
+            """,
+            {"regime_id": regime_id},
+        )
+
+        results = self.cursor.fetchall()
+        return results
+
+    def get_exercise_id(self, exercise_name):
+        self.cursor.execute(
+            """
+            SELECT exercises_id
+            FROM exercises_tb
+            WHERE name = :exercise_name
+            """,
+            {
+                "exercise_name": exercise_name,
+            },
+        )
+        results = self.cursor.fetchone()
+        return results[0]
+
+    def get_regime_id_2(self, date, patient_id):
+        self.cursor.execute(
+            """
+            SELECT exercise_regime_id
+            FROM exercise_regime_tb
+            WHERE date = :date AND patient_id = :patient_id
+            """,
+            {"date": date, "patient_id": patient_id},
+        )
+        results = self.cursor.fetchone()
+        return results[0]
+
+    def get_clinician_type(self, clinician_id):
+        self.cursor.execute(
+            """
+            SELECT clinician
+            FROM clinician_tb
+            WHERE staff_id = :clinician_id
+            """,
+            {"clinician_id": clinician_id},
+        )
+        results = self.cursor.fetchone()
+        return results[0]
+
+    def get_time(self, appointment_id, clinician_id):
+        self.cursor.execute(
+            """
+            SELECT time
+            FROM appointment_tb
+            WHERE appointment_id = :appointment_id 
+            """,
+            {"appointment_id": appointment_id, "clinician_id": clinician_id},
+        )
+        results = self.cursor.fetchone()
+        return results[0]
